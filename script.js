9ced3034d3c938193eea0f5a7fcf6247f4317267
akshaymondal528 <akshaymondal528@gmail.com>
2021-12-27 20:40:36 +0530

    run all test cases

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const model = document.querySelector('.model');
+const overlay = document.querySelector('.overlay');
+const btnOpenModel = document.querySelectorAll('.show-model');
+const btnCloseModel = document.querySelector('.close-model');
+
+const openModel = () => {
+  model.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModel = () => {
+  model.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnOpenModel.length; i++) {
+  btnOpenModel[i].addEventListener('click', openModel);
+}
+
+btnCloseModel.addEventListener('click', closeModel);
+overlay.addEventListener('click', closeModel);
